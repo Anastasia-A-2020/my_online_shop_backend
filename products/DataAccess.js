7763ff2471e79aa1e243bfe9a98cef18149bb621
dvc.develper1@gmail.com <dvc.develper1@gmail.com>
2025-02-03 15:41:39 +0200

    add dotenv, update faile .env

---
+++ products/DataAccess.js
@@ -0,0 +1,1 @@
+import ProductModel from "./ProductModel.js";
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 import chalk from "chalk";
+import dotenv from "dotenv";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
 const main = async () => {
-  await mongoose.connect(
-    "mongodb+srv://test:test@cluster0.cihqd.mongodb.net/online)shop?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  dotenv.config();
+
+  const mongoURI = process.env.MONGO_URI;
+
+  await mongoose.connect(mongoURI);
 
   console.log(chalk.magenta("Connect to MongoDB..."));
 
